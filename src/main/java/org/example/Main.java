840f5e25733249052e9837671267ec2a14c7d139
jacobklitgaard <jacobklitgaardhansen@gmail.com>
2026-02-12 15:06:08 +0100

    Revert "tomt projekt"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,20 +2,7 @@ package org.example;
 
 public class Main {
     static void main() {
-        int number = "10";                     // Fejl 1
-        System.out.println("Number is: " + number)
 
-        if (number > 5) {                      // Fejl 2
-            System.out.println("Number is big");
-        }
-
-        for (int i = 0; i < 5; i++)            // Fejl 3
-            System.out.println(i);
-
-        String name = null;
-        System.out.println(name.length());     // Fejl 4
-
-        int[] numbers = new int[3];
-        numbers[3] = 7;
     }
 }
+
